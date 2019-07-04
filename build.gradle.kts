3c3cf0f5ac2d7c00fc89244d11be1c10120e43b7
Nicholas Christopher <nwillc@gmail.com>
2019-07-04 15:57:10 -0400

    bintray dist

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
+import com.jfrog.bintray.gradle.BintrayExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val jvmTargetVersion = JavaVersion.VERSION_1_8.toString()
+val publicationName = "maven"
 
 val assertjVarsion: String by project
 val detektToolVersion: String by project
@@ -12,14 +14,16 @@ val ktlintVersion: String by project
 plugins {
     kotlin("jvm") version "1.3.41"
     jacoco
+    `maven-publish`
     id("org.jetbrains.dokka") version "0.9.18"
     id("org.jlleitschuh.gradle.ktlint") version "8.1.0"
     id("com.github.nwillc.vplugin") version "2.3.0"
     id("io.gitlab.arturbosch.detekt") version "1.0.0-RC16"
+    id("com.jfrog.bintray") version "1.8.4"
 }
 
 group = "com.github.nwillc"
-version = "0.5.0-SNAPSHOT"
+version = "0.5.0"
 
 logger.lifecycle("${project.group}.${project.name}@${project.version}")
 
@@ -46,6 +50,51 @@ detekt {
 
 jacoco {
     toolVersion = jacocoToolVersion
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    classifier = "sources"
+    from(sourceSets["main"].allSource)
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    dependsOn("dokka")
+    classifier = "javadoc"
+    from("$buildDir/javadoc")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>(publicationName) {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            from(components["java"])
+            artifact(sourcesJar.get())
+            artifact(javadocJar.get())
+        }
+    }
+}
+
+bintray {
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_API_KEY")
+    dryRun = false
+    publish = true
+    setPublications(publicationName)
+    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+        repo = publicationName
+        name = project.name
+        desc = "Functional Kotlin JDBC Extensions."
+        websiteUrl = "https://github.com/nwillc/${project.name}"
+        issueTrackerUrl = "https://github.com/nwillc/${project.name}/issues"
+        vcsUrl = "https://github.com/nwillc/${project.name}.git"
+        version.vcsTag = "v${project.version}"
+        setLicenses("ISC")
+        setLabels("kotlin", "JDBC")
+        publicDownloadNumbers = true
+    })
 }
 
 tasks {
